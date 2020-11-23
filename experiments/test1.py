9dae54d36bc37c0612f9c9a2bdddcc69812b49c5
bhgt <bhagat.nagi@gmail.com>
2020-11-23 10:03:37 +0530

    Update test1.py

--- experiments/test1.py
+++ experiments/test1.py
@@ -7,6 +7,8 @@ import os
 
 minValue = 50
 cap = cv2.VideoCapture(0)
+capturing = False
+timer = 0
 
 while True:
     ret, frame = cap.read()
@@ -18,20 +20,23 @@ while True:
     ret, thres = cv2.threshold(adaptThres, minValue, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     final = thres
 
-    #cv2.putText(final, 'henlo', (0, 0), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 1)
-
+    #cv2.putText(frame, 'henlo', (100, 100), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 0), 1)
+    if capturing:
+        timer += 0.5
+        if timer >= 50:
+            cv2.imwrite('capture.jpg', final)
+            capturing = False
+            print('captured image')
+        cv2.putText(final, str(int(50-timer)//10), (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 1)
+    
     cv2.imshow('frame', final)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite('capture.jpg', final)
-    if cv2.waitKey(1) & 0xFF == ord('z'):
-        minValue -= 10
-        print('minValue: ', minValue)
-    if cv2.waitKey(1) & 0xFF == ord('x'):
-        minValue += 10
-        print('minValue: ', minValue)
-
+        timer = 0
+        capturing = True
+        print('starting capture...')
+    
 cap.release()
 cv2.destroyAllWindows()
